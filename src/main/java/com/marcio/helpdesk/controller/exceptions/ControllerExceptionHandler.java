e43290135d25ef31af81d2575f038d4bac432b9f
Marcio Douglas <marciodouglas.camargo@gmail.com>
2023-03-28 12:54:28 -0300

    Validação nos campos de criação de novo usuário

--- src/main/java/com/marcio/helpdesk/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/marcio/helpdesk/controller/exceptions/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import com.marcio.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.marcio.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,6 +24,18 @@ public class ControllerExceptionHandler {
     public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, HttpServletRequest request){
+
+        ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Validation error", "Erro na validação dos campos", request.getRequestURI());
+
+        for(FieldError x : ex.getBindingResult().getFieldErrors()){
+            errors.addErrors(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
 }
--- src/main/java/com/marcio/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/marcio/helpdesk/domain/dtos/TecnicoDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.marcio.helpdesk.domain.Tecnico;
 import com.marcio.helpdesk.domain.enums.Perfil;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -14,9 +15,13 @@ public class TecnicoDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     protected Integer id;
+    @NotNull(message = "O campo NOME é requerido")
     protected String nome;
+    @NotNull(message = "O campo CPF é requerido")
     protected String cpf;
+    @NotNull(message = "O campo EMAIL é requerido")
     protected String email;
+    @NotNull(message = "O campo SENHA é requerido")
     protected String senha;
     protected Set<Integer> perfis = new HashSet<>();
     @JsonFormat(pattern = "dd/MM/yyyy")
